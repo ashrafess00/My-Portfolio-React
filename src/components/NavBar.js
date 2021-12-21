96fa3046f092e2bc300d943986cf67a8e42d39b7
Achraf Essaoudi <achrafess1937@gmail.com>
2021-12-21 17:20:16 +0100

    redirect to home when click contact me

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,32 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import logo from "../images/myLogo.svg";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+
 const NavBar = () => {
   const [humIcon, setHumIcon] = useState(false);
 
-  //   let humIcon = document.getElementById("humIcon")
-
-  // humIcon.addEventListener("click" , ()=>{
-  //     menu.classList.toggle("display")
-  // })
-
-  // console.log(contactMe);
+  let toHome = useNavigate();
 
   const scrollToContact = () => {
-    let contactMe = document.getElementById("contactMe");
-    window.scrollTo({
-      top: contactMe.offsetTop,
-      left: 0,
-      behavior: "smooth",
-    });
+    toHome("/");
+    scroll();
   };
 
+  async function scroll() {
+    let contactMe = document.getElementById("contactMe");
+    try {
+      window.scrollTo({
+        top: contactMe.offsetTop,
+        left: 0,
+        behavior: "smooth",
+      });
+    } catch (e) {
+      console.log("hi");
+    }
+  }
   return (
     <header className="flex jc-sb" id="navBar">
       <Link to="/">
